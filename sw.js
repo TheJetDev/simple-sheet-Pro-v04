bc3230773b694bd5d65ad08946d9004de92c3e03
TheJetDev <thejetdev.contact@gmail.com>
2026-01-03 16:47:48 +0900

    Update service worker cache version to v17

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // sw.js
-const CACHE_NAME = 'simple-calc-app-v16'; 
+const CACHE_NAME = 'simple-calc-app-v17'; 
 
 const urlsToCache = [
   './',
@@ -28,4 +28,3 @@ self.addEventListener('activate', event => {
 self.addEventListener('fetch', event => {
   event.respondWith(caches.match(event.request).then(res => res || fetch(event.request)));
 });
-
